bea3ffd6de3e2111e6c62f9062ddbdf44b681734
Iremide Beyioku <148532678+B3yioku@users.noreply.github.com>
2024-09-06 15:40:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save(){
+    let countDash = count + " - "
+    saveEl.textContent += countDash
+    
+    countEl.textContent = 0   
+    count -= count
+}
+
+    
